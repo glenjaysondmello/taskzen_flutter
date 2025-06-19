70091adb934d2a734f30bef5b0ad43acb263530f
Glen Jayson Dmello <glendmello04@gmail.com>
2025-06-19 20:05:42 +0530

    coounter' :

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:new_project/pages/first_page.dart';
-import 'package:new_project/pages/home_page.dart';
-import 'package:new_project/pages/profile_page.dart';
-import 'package:new_project/pages/settings_page.dart';
+import 'package:new_project/pages/counter_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +7,9 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-      routes: {
-        "/firstpage": (context) => FirstPage(),
-        "/homepage": (context) => HomePage(),
-        "/settingspage": (context) => SettingsPage(),
-        "/profilepage": (context) => ProfilePage(),
-      },
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: CounterPage());
   }
 }
