d1bcfd631b7b8936f09d0ac68fb3694a8c7a411a
Glen Jayson Dmello <glendmello04@gmail.com>
2025-07-22 19:58:17 +0530

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:new_project/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,6 +10,8 @@ void main() async {
   await Hive.initFlutter();
 
   var box = await Hive.openBox("mybox");
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
